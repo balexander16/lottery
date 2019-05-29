dbe2abf8fe565a976aa2c2290be04b4c0ec90c71
Brian <babta29@gmail.com>
2019-05-28 22:53:18 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Roadrunner.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+public class Roadrunner {
+
+  private static final int MAXIMUM_NUMBER = 37;
+  private static final int NUMBER_TO_PICK = 5;
+  private static final int NUMBER_OF_TICKETS = 10;
+  private static final String PICK_FORMAT =
+      "Ticket #%d: %s%n";
+  public static void main(String[] args) {
+  }
+
+}
